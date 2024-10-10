61a543b2dff9c07fa1054ba465d085e5882ccf20
matiszac <zcornilliac@gmail.com>
2024-10-09 21:25:35 -0400

    update leafnode and tests

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -12,18 +12,18 @@ value_a = 'This is a LINK'
 
 class TestLeafNode(unittest.TestCase):
     def test_html_no_tag(self):
-        node = LeafNode(value_raw)
+        node = LeafNode(None, value_raw)
         self.assertEqual(node.to_html(), value_raw)
 
     def test_html_a_tag(self):
-        node = LeafNode(value_a, 'a', props)
+        node = LeafNode('a', value_a, props)
         self.assertEqual(
             node.to_html(),
             f'<a href="https://github.com" target="_blank">{value_a}</a>'
         )
 
     def test_no_value(self):
-        node = LeafNode(None)
+        node = LeafNode(None, None)
         self.assertRaises(ValueError, node.to_html)
 
 
