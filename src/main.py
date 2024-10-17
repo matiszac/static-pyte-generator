b39b5c85d12fbd4c331109bea5086b71e66a6307
matiszac <zcornilliac@gmail.com>
2024-10-17 17:24:38 -0400

    now creates all pages in content folder regardless of directory structure

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+from pathlib import Path
 from htmlnode import ParentNode, LeafNode
 from markdown_blocks import markdown_to_html_node
 
@@ -29,15 +30,56 @@ Click [here](https://google.com)'''
 
 def main():
     root = '..' if 'main.py' in os.listdir(os.path.curdir) else '.'
-    public_directory = 'public'
-    content_directory = 'content'
+    prepare_public_folder(root)
+    
+    dir_path_content = os.path.join(root, 'content')
+    template_file_path = os.path.join(root, 'template.html')
+    dest_dir_path = os.path.join(root, 'public')
 
-    public_folder_path = os.path.join(root, public_directory)
-    content_folder_path = os.path.join(root, content_directory)
+    generate_pages_recursive(dir_path_content, template_file_path, dest_dir_path)
 
+
+
+
+# ---- main end
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    files = list(map(lambda path: Path(path), get_files(dir_path_content)))
+    md_files = list(filter(lambda file: file.suffix == '.md' , files))
+    if not len(md_files) > 0:
+        return
+    template = Path(template_path).read_text()
+    for md in md_files:
+        md_content = md.read_text()
+        title = extract_title(md_content)
+        html_content = markdown_to_html_node(md_content).to_html()
+        dest_content = template.replace('{{ Title }}', title).replace('{{ Content }}', html_content)
+        sub_folder = md.parent.name
+        if sub_folder == 'content':
+            dest_file = Path(os.path.join(dest_dir_path, 'index.html'))
+        else:
+            dest_folder = os.path.join(dest_dir_path, sub_folder)
+            dest_file = Path(os.path.join(dest_folder, 'index.html'))
+        dest_file.write_text(dest_content)
+
+def generate_page(source_path, template_path, destination_path):
+    print(f'Generating page from: {source_path} -> to: {destination_path} % using: {template_path}')
+    template_html = get_file_text_content(template_path)
+    index_md_content = get_file_text_content(source_path)
+
+    title = extract_title(index_md_content)
+    index_md_html = markdown_to_html_node(index_md_content).to_html()
+
+    template_html = template_html.replace('{{ Title }}', title).replace('{{ Content }}', index_md_html)
+    with open(destination_path, mode='w', encoding='utf-8') as f:
+        f.write(template_html)
+
+def prepare_public_folder(root):
+    public_folder_path = os.path.join(root, 'public')
     if os.path.exists(public_folder_path):
         print('public folder detected. deleting public folder and its contents.')
         shutil.rmtree(public_folder_path)
+    print('creating public folder')
     os.mkdir(public_folder_path)
 
     static_folder_path = os.path.join(root, 'static')
@@ -57,29 +99,15 @@ def main():
                 destination_file_path = os.path.join(public_folder_path, file_name)
                 shutil.copy(file, destination_file_path)
 
-    template_file_path = os.path.join(root, 'template.html')
-    index_md_file_path = os.path.join(content_folder_path, 'index.md')
-    destination_index_path = os.path.join(public_folder_path, 'index.html')
-
-    generate_page(index_md_file_path, template_file_path, destination_index_path)
-
-
-
-
-# ---- main end
-
-def generate_page(source_path, template_path, destination_path):
-    print(f'Generating page from: {source_path} -> to: {destination_path} % using: {template_path}')
-    template_html = get_file_text_content(template_path)
-    index_md_content = get_file_text_content(source_path)
-
-    title = extract_title(index_md_content)
-    index_md_html = markdown_to_html_node(index_md_content).to_html()
-
-    template_html = template_html.replace('{{ Title }}', title).replace('{{ Content }}', index_md_html)
-    with open(destination_path, mode='w', encoding='utf-8') as f:
-        f.write(template_html)
-
+    content_folder_path = os.path.join(root, 'content')
+    if os.path.exists(content_folder_path):
+        folders = get_folders(content_folder_path)
+        if len(folders) != 0:
+            print('sub-directories found in content folder. creating sub-directories in public folder.')
+            for folder in folders:
+                folder_name = folder.replace(content_folder_path+'/', '')
+                destination_folder_path = os.path.join(public_folder_path, folder_name)
+                os.mkdir(destination_folder_path)
 
 def extract_title(markdown):
     lines = markdown.split('\n')
